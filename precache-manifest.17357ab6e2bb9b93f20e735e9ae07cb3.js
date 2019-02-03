f25c8b32241e9bf3b0168736e04ac29c589b804f
twistezo <kolkolukasz@gmail.com>
2019-02-03 19:13:29 +0100

    Updates

---
+++ precache-manifest.17357ab6e2bb9b93f20e735e9ae07cb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "946a3d879e31e5ad28c8",
+    "url": "/online-shop/static/js/runtime~main.946a3d87.js"
+  },
+  {
+    "revision": "165d14abbbf83fed8229",
+    "url": "/online-shop/static/js/main.165d14ab.chunk.js"
+  },
+  {
+    "revision": "b1a8f59e79af4527921b",
+    "url": "/online-shop/static/js/1.b1a8f59e.chunk.js"
+  },
+  {
+    "revision": "165d14abbbf83fed8229",
+    "url": "/online-shop/static/css/main.b19eb468.chunk.css"
+  },
+  {
+    "revision": "0fc3d8d93f6d91c08f254cf8c906adf6",
+    "url": "/online-shop/index.html"
+  }
+];
