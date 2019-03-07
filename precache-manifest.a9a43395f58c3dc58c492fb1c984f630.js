4efa1ec4b6253beedd3828e7a57a67857b2c989f
twistezo <kolkolukasz@gmail.com>
2019-03-07 14:32:41 +0100

    Updates

--- precache-manifest.a9a43395f58c3dc58c492fb1c984f630.js
+++ precache-manifest.a9a43395f58c3dc58c492fb1c984f630.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/online-shop/static/js/runtime~main.946a3d87.js"
   },
   {
-    "revision": "d8591238c0c024363806",
-    "url": "/online-shop/static/js/main.d8591238.chunk.js"
+    "revision": "ac0021c6d5335d9ee002",
+    "url": "/online-shop/static/js/main.ac0021c6.chunk.js"
   },
   {
     "revision": "f9f2a1bdde90f8371a89",
     "url": "/online-shop/static/js/1.f9f2a1bd.chunk.js"
   },
   {
-    "revision": "d8591238c0c024363806",
-    "url": "/online-shop/static/css/main.29b90218.chunk.css"
+    "revision": "ac0021c6d5335d9ee002",
+    "url": "/online-shop/static/css/main.20066c23.chunk.css"
   },
   {
-    "revision": "70c507f60dea3d3c0d976444efa93e14",
+    "revision": "97a399fa5757a2154f05fb746f508ef6",
     "url": "/online-shop/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/online-shop/precache-manifest.4835253d56da0cab675eb1e4d8e4db7e.js"
+  "/online-shop/precache-manifest.a9a43395f58c3dc58c492fb1c984f630.js"
 );
 
 workbox.clientsClaim();
